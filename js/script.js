6e6f72abaf4e2155e2eeaebdb4662c63f1ce38cb
ismael-back <ismaelsktcore9898@hotmail.com>
2021-09-05 08:26:03 -0300

    Teste

--- js/script.js
+++ js/script.js
@@ -189,7 +189,7 @@ fetch("../test.json")
         function prod(){
             
 
-            if(dados === null){
+            if(dados.length == '0' || dados === null){
                 alert('Você tem que estar logado para onseguir adicionar um produto no carrinho')
                 window.open('loginECadastro(ModoE).html', '_self')
             }
